31d3ddbe6efd8aa2e277189ea5e509fa14c62e64
Jawad Nisar <jawwad.nissar@gmail.com>
2018-05-07 16:10:10 +0500

    frontend changes

---
+++ resources/views/thankyou.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('header')
+    <style id='activation-inline-css' type='text/css'>
+        .site-header{background-image:url({{ asset('public/images//WebPhotoHeader_01-1.jpg') }});}
+    </style>
+    <div class="site-header-wrapper">
+
+
+        <div class="site-title-wrapper">
+
+
+            <h1 class="site-title"><a href="http://groupregistration.net/" rel="home">Group Registration</a></h1>
+            <div class="site-description">Travel With Us</div>
+        </div><!-- .site-title-wrapper -->
+
+        <div class="hero">
+
+
+            <div class="hero-inner">
+
+
+                <div class="page-title-container">
+
+                    <header class="page-header">
+
+
+                        <h1 class="page-title">Prefrences</h1>
+
+
+                    </header><!-- .entry-header -->
+
+                </div><!-- .page-title-container -->
+
+            </div>
+
+        </div>
+
+    </div><!-- .site-header-wrapper -->
+
+@endsection
+
+
+@section('content')
+    <div class="container-fluid">
+        <div class="container-page">
+
+                <p style="font-size: larger">Thank you for submitting your registration</p>
+
+            </form>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    @include('layouts/script')
+    <script type="text/javascript">
+        $(document).on('click','.previous', function(e){
+            e.preventDefault();
+            previouspage('/');
+        });
+    </script>
+@endsection
